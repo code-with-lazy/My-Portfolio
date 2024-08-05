cda64d692ecdd2d7c57b7e0c2355d79868100d0d
Shubham <98813638+code-with-lazy@users.noreply.github.com>
2024-08-06 01:38:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.multi',{
+    strings : ['FrontEnd Developer', 'UI Designer' , 'Python Developer'],
+    typeSpeed:95,
+    backSpeed:60,
+    backDelay:800,
+    loop: true
+
+});
